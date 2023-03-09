5b74e0eb5043c53038d6f5c680c65ec11b6ab3ba
Fujairah Research Center <108390870+FujairahResearchCenter@users.noreply.github.com>
2023-03-09 09:56:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import dash_table
-
-
+import numpy as np #extra
+import pylab #extra
 import pandas as pd
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dash_app.title = 'FRC-Camel-Race'
 app = dash_app.server
 
 colors = {"graphBackground": "#F5F5F5", "background": "#ffffff", "text": "#000000"}
@@ -55,6 +56,67 @@ dash_app.layout = html.Div(
 )
 
 
+def init(x, lag, threshold, influence):
+    '''
+    Smoothed z-score algorithm
+    Implementation of algorithm from https://stackoverflow.com/a/22640362/6029703
+    '''
+
+    labels = np.zeros(lag)
+    filtered_y = np.array(x[0:lag])
+    avg_filter = np.zeros(lag)
+    std_filter = np.zeros(lag)
+    var_filter = np.zeros(lag)
+
+    avg_filter[lag - 1] = np.mean(x[0:lag])
+    std_filter[lag - 1] = np.std(x[0:lag])
+    var_filter[lag - 1] = np.var(x[0:lag])
+
+    return dict(avg=avg_filter[lag - 1],
+                var=var_filter[lag - 1],
+                std=std_filter[lag - 1],
+                filtered_y=filtered_y,
+                labels=labels)
+
+def add(result, single_value, lag, threshold, influence):
+    previous_avg = result['avg']
+    previous_var = result['var']
+    previous_std = result['std']
+    filtered_y = result['filtered_y']
+    labels = result['labels']
+
+    if abs(single_value - previous_avg) > threshold * previous_std:
+        if single_value > previous_avg:
+            labels = np.append(labels, 1)
+        else:
+            labels = np.append(labels, -1)
+
+        # calculate the new filtered element using the influence factor
+        filtered_y = np.append(filtered_y, influence * single_value
+                               + (1 - influence) * filtered_y[-1])
+    else:
+        labels = np.append(labels, 0)
+        filtered_y = np.append(filtered_y, single_value)
+
+    # update avg as sum of the previuos avg + the lag * (the new calculated item - calculated item at position (i - lag))
+    current_avg_filter = previous_avg + 1. / lag * (filtered_y[-1]
+            - filtered_y[len(filtered_y) - lag - 1])
+
+    # update variance as the previuos element variance + 1 / lag * new recalculated item - the previous avg -
+    current_var_filter = previous_var + 1. / lag * ((filtered_y[-1]
+            - previous_avg) ** 2 - (filtered_y[len(filtered_y) - 1
+            - lag] - previous_avg) ** 2 - (filtered_y[-1]
+            - filtered_y[len(filtered_y) - 1 - lag]) ** 2 / lag)  # the recalculated element at pos (lag) - avg of the previuos - new recalculated element - recalculated element at lag pos ....
+
+    # calculate standard deviation for current element as sqrt (current variance)
+    current_std_filter = np.sqrt(current_var_filter)
+
+    return dict(avg=current_avg_filter,
+                var=current_var_filter,
+                std=current_std_filter,
+                filtered_y=filtered_y[1:],
+                labels=labels)
+
 '''# Define callback
 @dash_app.callback(
     dash.dependencies.Output('graph', 'figure'),
@@ -81,6 +143,7 @@ def update_graph(contents, filename,selected_column):
     
     df = pd.DataFrame(columns =['Time',"Parameter"])
     parameter = "Parameter"
+    rt = 'Graph'
     if contents and selected_column:
         contents = contents[0]
         filename = filename[0]
@@ -97,10 +160,45 @@ def update_graph(contents, filename,selected_column):
         df.Time = df.Time.dt.strftime('%H:%M:%S.%f')
         parameter = selected_column
 
+        lag = 30
+        threshold = 5
+        influence = 0
+        if 'HR' in df.columns:
+            df = df[df.HR != 0]
+            y = df.HR.tolist()
+            indm = len(y) - y[::-1].index(max(y)) - 1 #y[np.argmax(y)]
+            # Run algo with settings from above
+            result = init(y[:lag], lag=lag, threshold=threshold, influence=influence)
 
+            for i in y[lag:]:
+                result = add(result, i, lag, threshold, influence)
+
+            indc = list(np.where(result['labels'][:-1] != result['labels'][1:])[0]) #changes
+            
+            cls = min(indc, key=lambda x:abs(x-indm)) #nearest value
+            if cls> indm:
+                normal = indc[indc.index(cls)-1]
+                after = cls
+            elif cls < indm:
+                normal = cls
+                after = indc[indc.index(cls)+1]
+            else:
+                normal = cls
+                after = cls
+
+                
+
+            kf = df.iloc[[normal,indm,after]]
+            #fig2 = px.scatter(df, x="Time", y="HR")
+            #kf.to_excel("kf.xlsx")
+            rt = "The heart recovery rate is : "+str(round(kf['MS'].iloc[2]-kf['MS'].iloc[1],3))+" Seconds"
+
+
+
+                         
     
     
-        fig = px.line(df, x='Time', y=parameter)
+        fig = px.line(df, x='Time', y=parameter,,title=rt)
         # plot only Google data for year 2018 with range slider
         fig = px.line(df, x='Time', y=parameter)
         fig.update_xaxes(
@@ -115,6 +213,10 @@ def update_graph(contents, filename,selected_column):
                 ])
             )
         )
+        if "HR" in df.columns:
+            fig.add_trace(go.Scatter(x=kf.Time.tolist(), y=kf.HR.tolist(),
+                         marker_symbol = 'star',marker_color=kf.HR.tolist(),text=['Before Race','At Peak', 'After Race'],mode = 'markers',
+                         marker_size = 15))
         fig.update_layout(
             autosize=False,
             width=1700,
@@ -227,7 +329,8 @@ def update_stat(contents, filename):
         filename = filename[0]
         df = parse_data(contents, filename)
         df = df.reset_index(drop=True)
-        
+        if 'HR' in df.columns:
+            df = df[df.HR != 0]        
         af = df.describe()
         af['Parameters'] = af.index
 
